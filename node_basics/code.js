9ba78ce665079865fd4ec9f5b61ae7a4a6c00156
Manoranjan Sethi <84295391+Manoranjan-Sethi@users.noreply.github.com>
2022-11-19 22:17:43 +0530

    basics-node

---
+++ node_basics/code.js
@@ -0,0 +1,19 @@
+//add
+function add(a, b) {
+  return a + b;
+}
+//array
+let arr = [1, 4, 7, 8, 2, 5];
+
+//object
+let obj = { a: "gi", n: 34 };
+
+//strings
+var str = "1sd45g";
+
+module.exports = {
+  arr,
+  obj,
+  str,
+  add,
+};
---
+++ node_basics/index.js
@@ -0,0 +1,8 @@
+console.log("Hello world!");
+
+const { arr, obj, str, add } = require("./code");
+
+console.log(obj);
+console.log(arr);
+console.log(str);
+console.log(add(2, 7));
