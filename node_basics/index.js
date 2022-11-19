67dd735e36cb033d4359f490cac307c3a60bf6e8
Manoranjan Sethi <84295391+Manoranjan-Sethi@users.noreply.github.com>
2022-11-19 23:02:27 +0530

    fin

--- node_basics/index.js
+++ node_basics/index.js
@@ -6,3 +6,27 @@ console.log(obj);
 console.log(arr);
 console.log(str);
 console.log(add(2, 7));
+
+//file system
+const fs = require("fs");
+//sync way to write which is not recommended
+const read = fs.readFileSync("./test.txt", { encoding: "utf-8" });
+console.log(read);
+
+// use async always
+fs.readFile("./test.txt", { encoding: "utf-8" }, (err, data) => {
+  if (err) {
+    console.log(err);
+  }
+  console.log(data);
+});
+
+console.log("reading test file");
+
+//operating system
+const os = require("os");
+const cpuData = os.cpus();
+console.log(cpuData);
+console.log(cpuData.length); // number of cores
+console.log(cpuData.map((e) => e.speed)); //every core speed
+console.log(os.platform()); //windows platform
---
+++ node_basics/tempCodeRunnerFile.js
@@ -0,0 +1,2 @@
+const read = fs.readFileSync("./test.txt", { encoding: "utf-8" });
+// console.log(read);
