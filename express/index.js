248e944951dd9631895001a6e619d30b719105b1
Manoranjan Sethi <84295391+Manoranjan-Sethi@users.noreply.github.com>
2022-11-22 01:12:04 +0530

    bs

--- express/index.js
+++ express/index.js
@@ -1,6 +1,7 @@
 const express = require("express");
 
 const app = express();
+const fs = require("fs");
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
@@ -10,14 +11,66 @@ app.get("/", (req, res) => {
   res.end("World!");
 });
 
+//reading products
 app.get("/products", (req, res) => {
-  res.end(JSON.stringify({ a: "a", b: "b" }));
+  fs.readFile("./db.json", { encoding: "utf-8" }, (err, data) => {
+    res.end(data);
+  });
 });
 
+//adding products
 app.post("/products", (req, res) => {
   // write body in db.json
+  fs.readFile("./db.json", { encoding: "utf-8" }, (err, data) => {
+    const parse = JSON.parse(data); // converting data as strings to valid JSON objects
+    parse.products = [...parse.products, req.body]; //adding new products to the array
 
-  res.end("got data");
+    fs.writeFile(
+      "./db.json",
+      JSON.stringify(parse),
+      { encoding: "utf8" },
+      () => {
+        res.end("New data added");
+      }
+    );
+  });
+});
+
+//update products
+app.put("/products/:id", (req, res) => {
+  // write body in db.json
+  const { id } = req.params;
+  fs.readFile("./db.json", { encoding: "utf-8" }, (err, data) => {
+    const parse = JSON.parse(data);
+    parse.products = parse.products.map((el) => el.id == id);
+
+    fs.writeFile(
+      "./db.json",
+      JSON.stringify(parse),
+      { encoding: "utf-8" },
+      () => {
+        res.end("New data added");
+      }
+    );
+  });
+});
+
+//delete products
+app.delete("/products/:id", (req, res) => {
+  const { id } = req.params;
+  fs.readFile("./db.json", { encoding: "utf-8" }, (err, data) => {
+    const parse = JSON.parse(data);
+    parse.products = parse.products.filter((el) => el.id !== id);
+
+    fs.writeFile(
+      "./db.json",
+      JSON.stringify(parse),
+      { encoding: "utf-8" },
+      () => {
+        res.status(201).end("Product Deleted");
+      }
+    );
+  });
 });
 
 app.listen(8090, () => {
